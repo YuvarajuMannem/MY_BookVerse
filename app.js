2eee7c438e7f2fdb102d5aa211997a6ede7af78a
Yuvaraju Mannem <mannemyuvaraju9503@gmail.com>
2025-08-09 14:46:40 +0530

    removed static serving

--- app.js
+++ app.js
@@ -30,10 +30,12 @@ const Book = require('./models/Book');
 const authRoutes = require('./routes/auth');
 const bookRoutes = require('./routes/books');
 
+app.use('/api',(req,res)=>{
+  return res.json({ message: 'BookVerse API up' });
+})
 app.use('/api/auth', authRoutes);
 app.use('/api/books', bookRoutes);
 
-app.use(express.static('public')); // serve frontend from public folder
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
